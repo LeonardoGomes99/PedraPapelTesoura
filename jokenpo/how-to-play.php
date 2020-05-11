a5a302710cd105e274ddc75ee993bfe43dd9986e
Leonardo Gomes Da Silva <35742346+LeonardoGomes99@users.noreply.github.com>
2020-05-10 21:48:28 -0300

    Att#Final
    Att#Final

---
+++ jokenpo/how-to-play.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+				<meta name="Leonardo Gomes da Silva" content="">
+					<title>Jogo de Jo-Ken-Po</title>
+					<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+						<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+							<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+								<link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+									<link href="css/stylish-portfolio.min.css" rel="stylesheet">
+									</head>
+									<body id="page-top">
+										<header class="masthead d-flex">
+											<div class="container text-center my-auto">
+												<a class="btn btn-primary btn-xl js-scroll-trigger" href="index.php">Página Principal</a>
+												<h1 class="mb-1">Tutorial</h1>
+												<video width="300" controls>
+													<br>
+													<source src="img/tutorial.mp4" type="video/mp4">
+                           							Your browser does not support HTML video.
+      
+													</video>
+													<br>
+														<a class="btn btn-primary btn-xl js-scroll-trigger" href="lizard-spock-default.php">Aprendeu? Bora jogar</a>
+														<br>
+															<p>All this video material belongs to Warner Bros. Television Distribution
+																<p>
+																	<br>
+																	</div>
+																	<div class="overlay"></div>
+																</header>
+																<script src="vendor/jquery/jquery.min.js"></script>
+																<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+																<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+															</body>
+														</html>
---
+++ jokenpo/lizard-spock-default.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+				<meta name="Leonardo Gomes da Silva" content="">
+					<title>Jogo de Jo-Ken-Po</title>
+					<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+						<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+							<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+								<link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+									<link href="css/stylish-portfolio.min.css" rel="stylesheet">
+									</head>
+									<body id="page-top">
+										<header class="masthead d-flex">
+											<div class="container text-center my-auto">
+												<a class="btn btn-primary btn-xl js-scroll-trigger" href="index.php">Página Principal</a>
+												<a class="btn btn-primary btn-xl js-scroll-trigger" href="how-to-play.php">Não sabe jogar? Clique Aqui!</a>
+															<br>
+															<br>
+															<h2 class="mb-1">ESCOLHA UMA OPÇÃO</h1>
+															<br>															
+																	<form action="resultado-lizard-spock-default.php" method="POST">
+																		<div class="btn btn-dark btn-xl js-scroll-trigger">
+																			<img src="img/paper.png">
+																				<br>
+																					<input class="btn btn-dark btn-xl js-scroll-trigger" type="radio" name="choice" value="Papel"  />
+																				</div>
+																				<div class="btn btn-dark btn-xl js-scroll-trigger">
+																					<img src="img/rock.png">
+																						<br>
+																							<input class="btn btn-dark btn-xl js-scroll-trigger" type="radio" name="choice" value="Pedra" />
+																						</div>
+																						<br>
+																						<br>
+																						<div class="btn btn-dark btn-xl js-scroll-trigger">
+																							<img src="img/scissor.png">
+																								<br>
+																									<input class="btn btn-dark btn-xl js-scroll-trigger" type="radio" name="choice" value="Tesoura" required/>
+																								</div>
+																								<div class="btn btn-dark btn-xl js-scroll-trigger">
+																									<img src="img/lizard.png">
+																										<br>
+																											<input class="btn btn-dark btn-xl js-scroll-trigger" type="radio" name="choice" value="Lagarto"  />
+																										</div>
+																										<br>
+																										<br>
+																										<div class="btn btn-dark btn-xl js-scroll-trigger">
+																											<img src="img/spock.png">
+																												<br>
+																													<input class="btn btn-dark btn-xl js-scroll-trigger" type="radio" name="choice" value="Spock"/>
+																												</div>
+																												<br>
+																													<br>											
+																														<input class="btn btn-primary btn-xl js-scroll-trigger" id="radioValidate" type="submit" value="JOGAR!">
+																														</form>
+																														<br>
+																															<br>																																		
+																																</div>
+																																<div class="overlay"></div>
+																															</header>
+																															<a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+																																<i class="fas fa-angle-up"></i>
+																															</a>
+																															<script src="vendor/jquery/jquery.min.js"></script>
+																															<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+																															<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+																															<script src="js/stylish-portfolio.min.js"></script>
+																														</body>
+																													</html>
